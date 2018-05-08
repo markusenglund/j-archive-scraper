7a74be1829996426f2155c40127390d8ea00328f
Markus <markus.s.englund@gmail.com>
2018-05-08 11:44:28 +0200

    calculate relevant category, round and value for every clue

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const fetchGameData = gameId => {
     .get(`http://www.j-archive.com/showgame.php?game_id=${gameId}`)
     .then(({ data }) => {
       const $ = cheerio.load(data);
+
       const categories = [];
       $(".category_name").each(function() {
         categories.push($(this).text());
@@ -20,10 +21,27 @@ const fetchGameData = gameId => {
       console.log(categories);
 
       const clues = [];
-      $(".clue_text").each(function() {
-        clues.push($(this).text());
+      $(".clue_text").each(function(i) {
+        const clue = $(this).text();
+        let category;
+        let round;
+        let value;
+        if (i < 30) {
+          category = categories[i % 6];
+          round = "j";
+          value = Math.floor(i / 6 + 1) * 200;
+        } else if (i < 60) {
+          category = categories[i % 6 + 6];
+          round = "dj";
+          value = Math.floor((i - 30) / 6 + 1) * 400;
+        } else if (i === 60) {
+          category = categories[12];
+          round = "fj";
+        }
+
+        clues.push({ clue, category, round, value });
       });
-      console.log(clues);
+      // console.log(clues);
     });
 };
 
